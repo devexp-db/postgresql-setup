f66016aefcfde5602e289226d2242e18d6eab6bf
Petr Khartskhaev <pkhartsk@redhat.com>
2026-03-11 15:06:29 +0100

    Add data checksums
    # Conflicts:
    #	bin/postgresql-setup.in

---
+++ tmt/tests/virtual/checksums/pre-pg18/test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+  rlPhaseStartSetup
+    rlRun "git clone \"$REPO_URL\" repo"
+    rlRun "pushd repo"
+    rlRun "git fetch origin \"$PR_HEAD\""
+    rlRun "git checkout FETCH_HEAD"
+    rlRun "cat /etc/fedora-release"
+    rlRun "dnf -y install postgresql16-server"
+
+    rlRun "autoreconf -vfi" 0 "Building and installing postgresql-setup"
+    rlRun "./configure --prefix=/usr"
+    rlRun "make"
+    rlRun "./bin/postgresql-setup --init" 0 "Initializing database dir"
+    rlRun "systemctl start postgresql" 0 "Starting service"
+    rlRun "systemctl is-active postgresql" 0 "Verifying service running"
+  rlPhaseEnd
+
+  rlPhaseStartTest
+    rlRun "su - postgres -c \"
+    createdb testdb;
+    psql -U postgres -d testdb -c \\\"create table users (id serial primary key, name text)\\\";
+    psql -U postgres -d testdb -c \\\"insert into users (name) values ('Alice'), ('Bob'), ('Celine')\\\"
+    \""
+    rlRun "su - postgres -c '
+    psql -U postgres -d testdb -c \"select * from users\"
+    ' > expected.txt"
+
+    echo "Expected:"
+    cat expected.txt
+
+    rlRun "dnf -y remove postgresql16*" 0 "Removing postgresql 16"
+    rlRun "dnf -y install postgresql17-upgrade" 0 "Installing postgresql 17"
+
+    rlRun "./bin/postgresql-setup --upgrade --data-checksums" 0 "Upgrading with checksums flag"
+
+    rlRun "systemctl start postgresql" 0 "Starting service again"
+    rlRun "systemctl is-active postgresql" 0 "Verifying service running"
+
+    rlRun "su - postgres -c '
+    psql -U postgres -d testdb -c \"select * from users\"
+    ' > actual17.txt"
+
+    echo "Actual:"
+    cat actual17.txt
+
+    rlAssertNotDiffer expected.txt actual17.txt
+    rlRun "pg_checksums /var/lib/pgsql/data" 0 "Verify checksums enabled"
+  rlPhaseEnd
+
+  rlPhaseStartCleanup
+    rlRun "popd"
+    rlRun "rm -rf repo"
+    rlRun "systemctl stop postgresql" 0 "Stopping postgresql service"
+    rlRun "rm -rf /var/lib/pgsql" 0 "Removing database folder"
+    rlRun "dnf -y remove postgresql*" 0 "Uninstalling postgresql"
+  rlPhaseEnd
+rlJournalPrintText
+rlJournalEnd
